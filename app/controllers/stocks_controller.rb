fa7fe6e54c73d5cbc309fb3181b1f75fd4bb7547
GuillaumeJapan <mauguen.guillaume@gmail.com>
2021-06-09 12:49:32 +0900

    Javascript response to invalid search

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,17 +2,14 @@ class StocksController < ApplicationController
   def search
     if (params[:stock]).present?
       @stock = Stock.new_lookup(params[:stock])
-      if @stock
-        respond_to do |format|
-          format.js { render partial: 'users/result' }
-        end
-      else
-        flash[:alert] = "Please enter a valid symbol"
-        redirect_to my_portfolio_path
+      if @stock.nil?
+        flash.now[:alert] = "Please enter a valid symbol to search"
       end
     else
-      flash[:alert] = "Please enter a symbol to search"
-      redirect_to my_portfolio_path
+      flash.now[:alert] = "Please enter a symbol to search"
+    end
+    respond_to do |format|
+      format.js { render partial: 'users/result' }
     end
   end
 end
