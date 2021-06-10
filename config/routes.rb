d1de5e81d1f51190d5f5f124a52655c9cb786571
GuillaumeJapan <mauguen.guillaume@gmail.com>
2021-06-10 11:48:47 +0900

    Improved Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   
   resources :user_stocks
-  #root 'welcome#index'
-  root 'users#my_portfolio'
+  root 'welcome#index'
   devise_for :users
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
