7c02122d5c568b8317ae5a9c1e6a01b661aad910
GuillaumeJapan <mauguen.guillaume@gmail.com>
2021-06-14 14:55:34 +0900

    Add untrack stock functionnality

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -21,4 +21,12 @@ class UserStocksController < ApplicationController
     redirect_to my_portfolio_path
   end
 
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(stock_id: stock.id, user_id: current_user.id).first
+    user_stock.destroy
+    flash[:notice] = "#{stock.name} was successfully removed from portfolio"
+    redirect_to my_portfolio_path
+  end
+
 end
