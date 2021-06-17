e9b95b8148505b82717866b706a5711b10b3ff2d
GuillaumeJapan <mauguen.guillaume@gmail.com>
2021-06-17 10:02:44 +0900

    Add self-referential association User<>Friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
+
   def my_portfolio
     @stocks = current_user.stocks
   end
+  
+  def my_friends
+    @friendss = current_user.friends
+  end
+
 end
