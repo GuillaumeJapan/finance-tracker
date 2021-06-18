017205c5e10edb50dd33226f929d70d9c9775f44
GuillaumeJapan <mauguen.guillaume@gmail.com>
2021-06-18 11:30:50 +0900

    Add spinner to stock search and fix bug no friends result

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,9 +15,15 @@ class UsersController < ApplicationController
   end
   
   def search
-    flash.now[:alert] = "Please enter a name or email to search" if !(params[:to_search]).present? 
-    @persons_found = User.search_for_friends(params[:to_search])
-    @persons_found.reject! {|user| user == current_user }
+    if (params[:to_search]).present?
+      @persons_found = User.search_for_friends(params[:to_search])
+      @persons_found.reject! {|user| user == current_user }
+      if @persons_found.empty?
+        flash.now[:alert] = "Couldn't find users"
+      end
+    else
+      flash.now[:alert] = "Please enter a name or email to search"  
+    end
     respond_to do |format|
       format.js { render partial: 'users/result_friends' }
     end
